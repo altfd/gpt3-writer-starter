0b5645e32a5deb2f2ca5e2d5044101da5a860047
JacobAF <jacob@altefide.com>
2022-12-30 21:15:14 +0530

    final update

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ const Home = () => {
     const { output } = data;
     console.log("OpenAI replied...", output.text);
 
-    setApiOutput("${output.text}");
+    setApiOutput(`${output.text}`);
     setIsGenerating(false);
   };
 
@@ -56,12 +56,33 @@ const Home = () => {
             onChange={onUserChangedText}
           />
           <div className="prompt-buttons">
-            <a className="generate-button" onClick={callGenerateEndpoint}>
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              onClick={callGenerateEndpoint}
+            >
               <div className="generate">
-                <p>Generate</p>
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
               </div>
             </a>
           </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
